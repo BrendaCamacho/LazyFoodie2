f0990ee151760f342da33092bd5ea31f0fcaac72
Charlie <caam_1275@hotmail.com>
2019-08-06 23:36:53 -0500

    fixing saved page for displaying saved cards

--- client/src/components/Saved/index.js
+++ client/src/components/Saved/index.js
@@ -13,35 +13,7 @@ render () {
                         <Logo />
                      </div>
              </div>
-              {/* <div className="row formContainer">
-              <div className="input-field col m8 l6 offset-m2 offset-l3">
-                 <form className="col s12 m8 l6">
-                        <label id="labelAdd" className="active" for="ingredients">I have this ingredient what can I cook?
-                        </label>
-                        <input id="ingredients" type="text" className="validate childflexInput" required     
-                        // onChange = { event => this.setState({query: event.target.value})} 
-                        // onKeyPress = { event => {
-                        // if(event.key == "Enter"){
-                        //     this.search();
-                        // }
-                        //  }}
-                          >    
 
-                     
-                           
-                        </input>
-                 
-                        <button id="addIngredients" className="btn waves-effect waves-light childFlexSearch" type="submit"
-                            name="action">Add<i className="material-icons right">add</i>
-                        </button>
-                
-                        </form>
-                     
-            </div>
-            </div> */}
-                 {/* <div className="row chipsRow col m8 l6">
-                      <div className="chip" data-name="milk">milk<i className="close material-icons">close</i></div>
-                </div>  */}
                 <div className="row">
                     <Card />
 
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 import Saved from "../components/Saved";
+import ResultsContainer from "../components/ResultsContainer";
 
-class SavedPage extends Component {
+class Saved extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            savedRecipes: []
+        }
+    }
+
     render() {
         return (
-            <Saved />
+            <div className="container">
+                <div className="row">
+                <ResultsContainer savedRecipes = {this.savedRecipes} path = {this.props.match.path}/>
+                </div>
+            </div>
         )
     }
 }
