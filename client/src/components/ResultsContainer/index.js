18abf84a1a1d12f977302509b02cf2bffc2be8bc
Charlie <caam_1275@hotmail.com>
2019-08-06 23:43:08 -0500

    solved search.js it was missing this.props.match.path for /

--- client/src/components/ResultsContainer/index.js
+++ client/src/components/ResultsContainer/index.js
@@ -4,34 +4,38 @@ import Card from "../Card";
 
 function ResultsContainer (props){
 
-        {/* Abre if de results > 0*/}    
-        if(props.recipesData.length > 0){
+    if(props.path === "/"){
+                {/* Abre if de results > 0*/}    
+                if(props.recipesData.length > 0){
 
-            return (
-                <div class="recipesContainer col s12">
+                    return (
+                        <div class="recipesContainer col s12">
+        
+                            {props.recipesData.map((results) => {
+                                const recipesInfo  = results.recipe
+                                return <Card 
+                                image = {recipesInfo.image}
+                                label = {recipesInfo.label}
+                                totalTime =  {recipesInfo.totalTime}
+                                calories = {recipesInfo.calories}
+                                yield = {recipesInfo.yield}
+                                url = {recipesInfo.url}
+                                ingredientLines  = {recipesInfo.ingredientLines}
+                                />
+                            })}
+        
+                        </div>
+        
+                    );
+                {/* Cierra if de results > 0 */}    
+                }else{
+                    return(
+                        <div></div>
+                    )
+                } {/* Cierra else de if results >0 */}
+    }
 
-                    {props.recipesData.map((results) => {
-                        const recipesInfo  = results.recipe
-                        return <Card 
-                        image = {recipesInfo.image}
-                        label = {recipesInfo.label}
-                        totalTime =  {recipesInfo.totalTime}
-                        calories = {recipesInfo.calories}
-                        yield = {recipesInfo.yield}
-                        url = {recipesInfo.url}
-                        ingredientLines  = {recipesInfo.ingredientLines}
-                        />
-                    })}
 
-                </div>
-
-            );
-        {/* Cierra if de results > 0 */}    
-        }else{
-            return(
-                <div></div>
-            )
-        } {/* Cierra else de if results >0 */}
 
 }
 
