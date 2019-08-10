baa7e787a0752ddc5329a25181f3eb9ba6cc5d20
Estefanía Vizcarra <estefaniavizcarra@MacBook-Air-de-Estefania-2.local>
2019-08-10 00:36:57 -0500

    chips not already displayed correctly

--- client/src/components/Chips/index.js
+++ client/src/components/Chips/index.js
@@ -4,42 +4,27 @@ import React, {Component} from 'react';
 
 class Chips extends Component {
    
-
-
     render() {
         return(
 
             <div className="row chipsRow col m8 l6">
+                {this.props.chipsName.map((searchChip) => {
 
-        {this.props.chipsName.map((searchChip) => {
-            return(
-                <div className="chip" data-name={searchChip}>
-                {searchChip}
 
-                <i className="close material-icons"    
-                 data-name={searchChip} onClick= {this.props.onClick}>
-                close  </i> 
+                            return(
+                            <div className="chip" data-name={searchChip}>
+                            {searchChip}
 
-    
-                </div>
-
-                
-                
+                            <i className="close material-icons" data-name={searchChip} onClick= {this.props.onClick}>close  </i> 
+                            </div>
                
-         )})
+                )})
+                }
 
-
-    }
-         </div>
-                
-
-
-        
-
+            </div>
         )
-    }
-}
 
-    
+   }
+}
 
     export default Chips;
